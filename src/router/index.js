ec6cf1c23fa1ce5f0fe44aaf80b4b5f961e82938
Udemezue John <johnzeus14@gmail.com>
2020-10-23 18:26:24 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+
+
+// In-app views
+
+
+
+ // The landing page Home, Password-Reset, Register and Login Views
+
+import Landing from '../views/Landing/Landing.vue'
+
+// The Hotel page Hotel list, Create, Update and Details
+
+import Hotels from '../views/Hotels/Hotels.vue'
+
+// The Chat page Chat list, Create, Update and Details
+
+import Chat from '../views/Chat/Chat.vue'
+
+// The Feeds page Feeds list, Create, Update and Details
+import Feeds from '../views/Feeds/Feeds.vue'
+
+// The Flight page Flight list, Create, Update and Details
+
+import Flight from '../views/Flight/Flight.vue'
+
+// The Guides  page Guides list, Create, Update and Details
+
+import Guides from '../views/Guides/Guides.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Landing',
+    component: Landing
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Landing/Landing.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-blue-100 h-full" >
+
+    	<p class="text-blue-900"> Hello world</p>
+      
+    </div>
+</template>
+
+<script>
+export default {
+
+ data(){
+
+  return{
+
+
+  }
+ },
+
+ methods:{
+
+
+ },
+
+}
+</script>
+
